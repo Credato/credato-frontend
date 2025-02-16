5373768fc2eae41857206470f126ac9894f7a211
Credato <limitlesstechnologiesllc@gmail.com>
2025-02-16 17:50:58 +0400

    Added Coming Soon page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", textAlign: "center" }}>
+          <h1>Coming Soon!</h1>
+          <p>We’re working hard to launch our platform. Stay tuned.</p>
+      </div>
   );
 }
-
 export default App;
